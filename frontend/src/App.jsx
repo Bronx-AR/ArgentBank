183470b76283b7373592f4edd08673efb03d58a5
Arnaud <73144518+Bronx-AR@users.noreply.github.com>
2024-05-08 00:17:28 +0200

    ajout des composants, pages, dossier, sass, utile au projet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/index";
+import Signin from "./pages/SingIn/index";
+import User from "./pages/User/index";
+import "./styles/index.scss";
+import Footer from "./components/Footer/Footer";
+import PrivateRoute from "./components/PrivateRoutes/PrivateRoute";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Signin />} />
+            <Route element={<PrivateRoute />}>
+              <Route path="/profile" element={<User />} />
+            </Route>
+          </Routes>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
